bb866fa0386911a40f7fd58f24302b42afe9fa9e
unknown <dannilorch@gmail.com>
2020-12-11 09:27:22 -0300

    Tabuada feita

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tabuada_La_os
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("Tabuada 1 a 10");
+            for(int i = 0; i < 11; ++i){
+
+                Console.WriteLine($"Tabuada do {i}");
+
+                for(var contad = 0; contad < 11; contad++){
+                    int resul = 1 * contad;
+                    Console.WriteLine($"{i} x {contad} = {resul}");
+                }
+
+            
+            }    Console.WriteLine("------------------");
+        }
+    }
+}
